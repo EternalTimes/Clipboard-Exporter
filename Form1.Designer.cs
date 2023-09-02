c4c56765cc28e98cf77fccd6c222bb9fd8cdfb5d
Eternal Times <quawtheph@qq.com>
2023-09-02 23:17:52 +0800

    更改文本

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,10 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            Name = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
