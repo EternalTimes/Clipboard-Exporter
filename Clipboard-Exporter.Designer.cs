99a6d8b83d582921b303651a3927cd78cfbe53cf
Eternal Times <quawtheph@qq.com>
2023-09-10 21:02:13 +0800

    Update UI

--- Clipboard-Exporter.Designer.cs
+++ Clipboard-Exporter.Designer.cs
@@ -29,54 +29,66 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(600, 400);
-            this.Text = "Clipboard Exporter";
-
-            this.clipboardHistoryTextBox = new System.Windows.Forms.TextBox();
-            this.clipboardHistoryTextBox.Location = new System.Drawing.Point(12, 12);
-            this.clipboardHistoryTextBox.Multiline = true;
-            this.clipboardHistoryTextBox.ReadOnly = true;
-            this.clipboardHistoryTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.clipboardHistoryTextBox.Size = new System.Drawing.Size(450, 300);
-
-            this.clearButton = new System.Windows.Forms.Button();
-            this.clearButton.Location = new System.Drawing.Point(12, 330);
-            this.clearButton.Size = new System.Drawing.Size(150, 30);
-            this.clearButton.Text = "清空";
-            this.clearButton.UseVisualStyleBackColor = true;
-            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
-
-            this.copyButton = new System.Windows.Forms.Button();
-            this.copyButton.Location = new System.Drawing.Point(175, 330);
-            this.copyButton.Size = new System.Drawing.Size(150, 30);
-            this.copyButton.Text = "复制";
-            this.copyButton.UseVisualStyleBackColor = true;
-            this.copyButton.Click += new System.EventHandler(this.copyButton_Click);
-
-            this.saveToFileButton = new System.Windows.Forms.Button();
-            this.saveToFileButton.Location = new System.Drawing.Point(338, 330);
-            this.saveToFileButton.Size = new System.Drawing.Size(150, 30);
-            this.saveToFileButton.Text = "保存到文件";
-            this.saveToFileButton.UseVisualStyleBackColor = true;
-            this.saveToFileButton.Click += new System.EventHandler(this.saveToFileButton_Click);
-
-            this.startStopButton = new System.Windows.Forms.Button();
-            this.startStopButton.Location = new System.Drawing.Point(501, 330);
-            this.startStopButton.Size = new System.Drawing.Size(90, 30);
-            this.startStopButton.Text = "开始监听";
-            this.startStopButton.UseVisualStyleBackColor = true;
-            this.startStopButton.Click += new System.EventHandler(this.startStopButton_Click);
-
-            this.clipboardMonitorTimer = new System.Windows.Forms.Timer(this.components);
-            this.clipboardMonitorTimer.Interval = 1000; // 设置定时器间隔为1秒
-            this.clipboardMonitorTimer.Tick += new System.EventHandler(this.clipboardMonitorTimer_Tick);
-
-            this.Controls.Add(this.clipboardHistoryTextBox);
-            this.Controls.Add(this.clearButton);
-            this.Controls.Add(this.copyButton);
-            this.Controls.Add(this.saveToFileButton);
-            this.Controls.Add(this.startStopButton);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Clipboard_Exporter));
+            clipboardHistoryTextBox = new TextBox();
+            clearButton = new Button();
+            copyButton = new Button();
+            saveToFileButton = new Button();
+            startStopButton = new Button();
+            clipboardMonitorTimer = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // clipboardHistoryTextBox
+            // 
+            resources.ApplyResources(clipboardHistoryTextBox, "clipboardHistoryTextBox");
+            clipboardHistoryTextBox.Name = "clipboardHistoryTextBox";
+            clipboardHistoryTextBox.ReadOnly = true;
+            // 
+            // clearButton
+            // 
+            resources.ApplyResources(clearButton, "clearButton");
+            clearButton.Name = "clearButton";
+            clearButton.UseVisualStyleBackColor = true;
+            clearButton.Click += clearButton_Click;
+            // 
+            // copyButton
+            // 
+            resources.ApplyResources(copyButton, "copyButton");
+            copyButton.Name = "copyButton";
+            copyButton.UseVisualStyleBackColor = true;
+            copyButton.Click += copyButton_Click;
+            // 
+            // saveToFileButton
+            // 
+            resources.ApplyResources(saveToFileButton, "saveToFileButton");
+            saveToFileButton.Name = "saveToFileButton";
+            saveToFileButton.UseVisualStyleBackColor = true;
+            saveToFileButton.Click += saveToFileButton_Click;
+            // 
+            // startStopButton
+            // 
+            resources.ApplyResources(startStopButton, "startStopButton");
+            startStopButton.Name = "startStopButton";
+            startStopButton.UseVisualStyleBackColor = true;
+            startStopButton.Click += startStopButton_Click;
+            // 
+            // clipboardMonitorTimer
+            // 
+            clipboardMonitorTimer.Interval = 1000;
+            clipboardMonitorTimer.Tick += clipboardMonitorTimer_Tick;
+            // 
+            // Clipboard_Exporter
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Dpi;
+            Controls.Add(clipboardHistoryTextBox);
+            Controls.Add(clearButton);
+            Controls.Add(copyButton);
+            Controls.Add(saveToFileButton);
+            Controls.Add(startStopButton);
+            Name = "Clipboard_Exporter";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
